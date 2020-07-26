1dce2a2aaacdb410f257d871771016129cf52d9f
watshever <louieweng@outlook.com>
2020-07-25 22:14:43 -0400

    initial commit

---
+++ application/scripts/script.js
@@ -0,0 +1,41 @@
+$("#scroll-button").click(function () {
+    $('html,body').animate({
+        scrollTop: $("#floor").offset().top
+    },
+        15000);
+});
+
+$(document).ready(function () {
+    $('.wp-about').waypoint(function () {
+        $('.wp-about').addClass('animate__animated animate__fadeInLeft');
+    }, {
+        offset: '75%'
+    })
+    $('.s1').waypoint({
+        handler: function (direction) {
+            if (direction == "up") {
+                $('.s1').addClass('animate__animated animate__fadeInLeft')
+            }
+        }
+    }, {
+        offset: '75%'
+    })
+    $('.s2').waypoint({
+        handler: function (direction) {
+            if (direction == "up") {
+                $('.s2').addClass('animate__animated animate__fadeInLeft')
+            }
+        }
+    }, {
+        offset: '75%'
+    })
+    $('.s3').waypoint({
+        handler: function (direction) {
+            if (direction == "up") {
+                $('.s3').addClass('animate__animated animate__fadeInLeft')
+            }
+        }
+    }, {
+        offset: '75%'
+    })
+})
